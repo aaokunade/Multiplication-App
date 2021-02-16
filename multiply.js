18aa0a53f6b9d1ed7132042cb310b6f2ab0c4952
aaokunade <okunaishat@gmail.com>
2021-02-16 20:55:39 +0000

    numbers now responsive from screen

--- multiply.js
+++ multiply.js
@@ -2,25 +2,61 @@ const inputsAns = document.querySelector(".ansInputs");
 const inputsRandom = document.querySelector(".randomInputs");
 const nextBtn = document.querySelector(".next")
 nextBtn.addEventListener("click", nextFunc);
+let inputBtn = document.querySelectorAll(".inputs");
+
+    inputBtn.forEach(function(btn){
+        btn.addEventListener("click", function(){
+            inputsAns.value = parseInt(`${inputsAns.value}${btn.value}`);
+            // console.log(inputsAns.value);
+        });
+    })
 
 let inputRandomA;
 let inputRandomB;
+
+
+
+let outputs1 = [
+    "Brilliant! Maa shaa Allah!",
+    "Amazing! Baaroka Allaah feek!",
+    "You are doing Great!",
+    "You are a genius, well-done!",
+]
+
+function outputs1Funct(arr){
+    outputs = Math.floor(Math.random() * outputs1.length);
+    return arr[outputs]
+}
+
+
+let outputs2 = [
+    "Oops, You can do better",
+    "Try again",
+    "You are close"
+]
+
+function outputs2Funct(arr){
+    outputs = Math.floor(Math.random() * outputs2.length);
+    return arr[outputs]
+}
+
 
 function multiply(){
     inputRandomA = Math.floor(Math.random() * 12 + 1);
     inputRandomB = Math.floor(Math.random() * 12 + 1);
     inputsRandom.value = `${inputRandomA} X ${inputRandomB}`;
+    // inputsRandom.value = inputRandomA * inputRandomB;
 }
 
 multiply();
 
 function nextFunc(){
     if (parseInt(inputsAns.value) === (inputRandomA * inputRandomB)){
-        alert("Ma Shaa Allah")
+        alert(outputs1Funct(outputs1));
         multiply();
         inputsAns.value = " ";
     }else {
-        alert("You can do Better")
+        alert(outputs2Funct(outputs2))
     }
 
 }
